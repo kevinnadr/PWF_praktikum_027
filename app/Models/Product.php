008b8d9fef744706f43ad3fb05eb35ed81389efe
Kevin Adrian <kevin19305.ib@gmail.com>
2026-03-11 13:08:20 +0700

    Pertemuan 3

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'qty', 'price', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function kategoris()
+    {
+        return $this->hasMany(Kategori::class);
+    }
+}
