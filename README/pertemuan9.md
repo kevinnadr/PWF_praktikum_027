9c77f27b9d747b10515f460bc9d77033788f4b56
Kevin Adrian <kevin19305.ib@gmail.com>
2026-05-08 20:01:17 +0700

    feat: implement API authentication with Sanctum and integrate Scramble for automated documentation

---
+++ README/pertemuan9.md
@@ -0,0 +1,29 @@
+# Pertemuan 9 - Pengujian API dengan Postman
+
+## API Product
+
+1. **GET Product**
+![GET Product](<SS pertemuan 9/GETProduct.png>)
+
+2. **POST Product**
+![POST Product](<SS pertemuan 9/POSTProduct.png>)
+
+3. **PUT/UPDATE Product**
+![PUT Product](<SS pertemuan 9/PUTProduct.png>)
+
+4. **DELETE Product**
+![DELETE Product](<SS pertemuan 9/DELETEProduct.png>)
+
+## API Category
+
+1. **GET Category**
+![GET Category](<SS pertemuan 9/GETPCategory.png>)
+
+2. **POST Category**
+![POST Category](<SS pertemuan 9/POSTCategory.png>)
+
+3. **PUT/UPDATE Category**
+![PUT Category](<SS pertemuan 9/PUTCategory.png>)
+
+4. **DELETE Category**
+![DELETE Category](<SS pertemuan 9/DELETECategory.png>)
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +27,15 @@ class AppServiceProvider extends ServiceProvider
 
         Gate::define('manage-category', function (User $user) {
             return $user->role === 'admin';
+        });
+
+        Scramble::configure()
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+            });
+
+        Gate::define('viewApiDocs', function () {
+            return true;
         });
     }
 }
